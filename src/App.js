75862f648a8c8d7509b4ce5b84e8bb82d1c69a40
dantaayy <dantaayy@gmail.com>
2023-02-17 15:07:34 -0700

    added map function to display pokemon card

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
+import { useState, useEffect } from 'react';
 
 const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
+	// initialize state
+	const [pokemonList, setPokemonList] = useState([]);
 
-  return (
-    <div data-testid="app">
-      <Navigation />
+	// Use effect to fetch the pokemon cards and avoid side effects
+	useEffect(() => {
+		fetch(pokeApi)
+			.then((res) => res.json())
+			.then((data) => {
+				setPokemonList(data.results);
+			})
+			.catch((error) => {
+				console.error(error)
+			})
+	}, []);
 
-      <h1>Pokemon should appear here</h1>
-      <PokemonCard />
-    </div>
-  );
+	return (
+		<div data-testid="app">
+			<Navigation />
+
+			<h1>Pokemon should appear here</h1>
+			{
+				// Map over pokemonList that is set by the useEffect
+				pokemonList.map((pokemon) =>
+					<PokemonCard name={pokemon.name} />
+				)
+			}
+
+		</div>
+	);
 }
 
 export { App };
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function PokemonCard({ url, name }) {
+	// initialize state
+	const [pokemon, setPokemon] = useState([]);
 
-  return (
-    <div>
-        pokemon card
-    </div>
-  );
+	// useEffect to fetch the data
+	useEffect(() => {
+		fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+			.then((res) => res.json())
+			.then((data) => {
+				setPokemon(data)
+			})
+			.catch((error) => {
+				console.error(error)
+			})
+	}, [name]);
+
+	return (
+		<div>
+			Pokemon Card
+		</div>
+	);
 }
 
 export { PokemonCard };
